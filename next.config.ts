be7e32323f4ba4e288c2bbc0ae73a6570b1a175d
Arshad-Johan <chinnadeva46@gmail.com>
2026-08-13 00:43:56 +0530

    frontend corrections redo

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: [
-    "*.trycloudflare.com",
-    "congratulations-simon-hit-liver.trycloudflare.com",
-    "*.loca.lt",
-    "localhost:3000"
-  ],
+  reactStrictMode: true,
 };
 
 export default nextConfig;
